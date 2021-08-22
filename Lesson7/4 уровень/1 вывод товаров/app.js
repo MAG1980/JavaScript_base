190cb5577486caacd6473e35f0ce2c876464cec6
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-08-22 21:02:55 +0300

    Закончил выполение ДЗ №7

--- Lesson7/4 уровень/1 вывод товаров/app.js
+++ Lesson7/4 уровень/1 вывод товаров/app.js
@@ -11,81 +11,86 @@
 */
 
 const products = {
-    phones: [
-        {
-            id: 1,
-            name: "Смартфон 1",
-            price: "23,99 р.",
-            imageUrl: "https://picsum.photos/seed/1/200",
-        },
-        {
-            id: 2,
-            name: "Смартфон 2",
-            price: "11,99 р.",
-            imageUrl: "https://picsum.photos/seed/2/200",
-        },
-        {
-            id: 3,
-            name: "Смартфон 3",
-            price: "22,99 р.",
-            imageUrl: "https://picsum.photos/seed/3/200",
-        },
-    ],
+  phones: [
+    {
+      id: 1,
+      name: "Смартфон 1",
+      price: "23,99 р.",
+      imageUrl: "https://picsum.photos/seed/1/200",
+    },
+    {
+      id: 2,
+      name: "Смартфон 2",
+      price: "11,99 р.",
+      imageUrl: "https://picsum.photos/seed/2/200",
+    },
+    {
+      id: 3,
+      name: "Смартфон 3",
+      price: "22,99 р.",
+      imageUrl: "https://picsum.photos/seed/3/200",
+    },
+  ],
 
-    tablets: [
-        {
-            id: 4,
-            name: "Планшет 4",
-            price: "99,99 р.",
-            imageUrl: "https://picsum.photos/seed/4/200",
-        },
-        {
-            id: 5,
-            name: "Планшет 5",
-            price: "44,99 р.",
-            imageUrl: "https://picsum.photos/seed/5/200",
-        },
-    ],
+  tablets: [
+    {
+      id: 4,
+      name: "Планшет 4",
+      price: "99,99 р.",
+      imageUrl: "https://picsum.photos/seed/4/200",
+    },
+    {
+      id: 5,
+      name: "Планшет 5",
+      price: "44,99 р.",
+      imageUrl: "https://picsum.photos/seed/5/200",
+    },
+  ],
 
-    tv: [
-        {
-            id: 6,
-            name: "Телевизор 6",
-            price: "199,99 р.",
-            imageUrl: "https://picsum.photos/seed/6/200",
-        },
-        {
-            id: 7,
-            name: "Телевизор 7",
-            price: "244,99 р.",
-            imageUrl: "https://picsum.photos/seed/7/200",
-        },
-        {
-            id: 8,
-            name: "Телевизор 8",
-            price: "399,99 р.",
-            imageUrl: "https://picsum.photos/seed/8/200",
-        },
-        {
-            id: 9,
-            name: "Телевизор 9",
-            price: "444,99 р.",
-            imageUrl: "https://picsum.photos/seed/9/200",
-        },
-    ],
+  tv: [
+    {
+      id: 6,
+      name: "Телевизор 6",
+      price: "199,99 р.",
+      imageUrl: "https://picsum.photos/seed/6/200",
+    },
+    {
+      id: 7,
+      name: "Телевизор 7",
+      price: "244,99 р.",
+      imageUrl: "https://picsum.photos/seed/7/200",
+    },
+    {
+      id: 8,
+      name: "Телевизор 8",
+      price: "399,99 р.",
+      imageUrl: "https://picsum.photos/seed/8/200",
+    },
+    {
+      id: 9,
+      name: "Телевизор 9",
+      price: "444,99 р.",
+      imageUrl: "https://picsum.photos/seed/9/200",
+    },
+  ],
 };
 
-
+let div = document.querySelector(".products");
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", clickHandler);
+});
 /**
  * Эта функция должна вызываться при клике по кнопкам.
  * @param {MouseEvent} event
  */
 function clickHandler(event) {
-    //вам нужно очищать содержимое .products
-    
-    //в showCategory надо передать строку с типом категории, тип берите
-    //из атрибута data-type у кнопки, по которой кликнули.
-    
+  //вам нужно очищать содержимое .products
+  div.innerHTML = "";
+  let category = event.target.dataset.type;
+  showCategory(category);
+  //в showCategory надо передать строку с типом категории, тип берите
+  //из атрибута data-type у кнопки, по которой кликнули.
 }
 
 /**
@@ -96,7 +101,10 @@ function clickHandler(event) {
  * по которой кликнули.
  */
 function showCategory(category) {
-    
+  products[category].forEach((product) => {
+    let markUp = getProductMarkup(product);
+    div.insertAdjacentHTML("beforeend", markUp);
+  });
 }
 
 /**
@@ -109,5 +117,10 @@ function showCategory(category) {
  * в верху этого файла.
  */
 function getProductMarkup(product) {
-
+  return `
+    <div>${product.name}</div>
+    <img src="${product.imageUrl}" alt="">
+    <div>${product.price}</div>
+   <a href="https://example.com/producs/${product.id}">Подробнее</a>
+   `;
 }
